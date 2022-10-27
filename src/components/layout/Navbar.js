ad3e101cffe3fa479ae9a611a49e3f2d0e500a25
Mi Seadi <michelesobrosa@gmail.com>
2022-10-27 16:26:20 -0300

    Add Conteúdo aba Cursos

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,6 +3,7 @@ import Container from "./Container";
 import styles from "./Navbar.module.css";
 import logo from "../../img/logo.png";
 
+<<<<<<< Updated upstream
 function Navbar() {
   return (
     <nav className={styles.navbar}>
@@ -27,6 +28,30 @@ function Navbar() {
       </Container>
     </nav>
   );
+=======
+function Navbar(){
+    return (
+        <nav className={styles.navbar}>
+            <Container>
+                <Link to='/'><img src={logo} alt="Zetta"/></Link>
+               <ul className={styles.list}>
+                <li className={styles.item}>
+                     <Link to="/">Home</Link>
+                </li>
+                <li className={styles.item}>
+                    <Link to="/about">Sobre</Link>
+                </li>
+                <li className={styles.item}>
+                <Link to="/courses">Cursos</Link>
+                </li>
+                <li className={styles.item}>
+                    <Link to="/contact">Contato</Link>
+                </li>
+               </ul>
+            </Container>
+        </nav>
+    )
+>>>>>>> Stashed changes
 }
 
 export default Navbar;
